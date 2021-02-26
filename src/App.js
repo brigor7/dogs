0949518bb4d4b2da63efa905e90a6388321812eb
=Bruno Igor <=brigor7@gmail.com>
2021-02-25 22:57:18 -0300

    Retirado biblioteca não utilizada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
