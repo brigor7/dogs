6305f91ae93127ad85bdd783c67fb2fc902d00dc
=Bruno Igor <=brigor7@gmail.com>
2021-03-04 19:47:02 -0300

    User: Alteração de rota de <Feed> para <Home>

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import UserHeader from './UserHeader';
 import { Routes, Route } from 'react-router-dom';
-import Feed from '../Feed/Feed';
 import UserStats from './UserStats';
 import UserPhotoPost from './UserPhotoPost';
+import Home from '../Home';
 
 const User = () => {
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Home />} />
         <Route path="/postar" element={<UserPhotoPost />} />
         <Route path="/estatisticas" element={<UserStats />} />
       </Routes>
