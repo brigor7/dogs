8af75c905c9233ddd0ca735b3aa72984c74fae2c
Bruno Igor <brigor7@gmail.com>
2021-03-09 16:04:53 -0300

    Create component <Image> e funcionalidade skeleton

--- src/components/Feed/FeedPhotosItem.js
+++ src/components/Feed/FeedPhotosItem.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import styles from './FeedPhotosItem.module.css';
+import React from 'react'
+import Image from '../../Helpers/Image'
+import styles from './FeedPhotosItem.module.css'
 
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   const handleClick = () => {
-    setModalPhoto(photo);
-  };
+    setModalPhoto(photo)
+  }
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
-  );
-};
+  )
+}
 
-export default FeedPhotosItem;
+export default FeedPhotosItem
