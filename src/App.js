789e284f8e3ee982bb774256277abe0641e1cdeb
Bruno Igor <brigor7@gmail.com>
2021-03-10 17:39:20 -0300

    Criação de página de view de foto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import Login from './components/login/Login';
-import { UserStorage } from './UserContext';
-import User from './components/Users/User';
-import ProtectedRoute from './Helpers/ProtectedRoute';
-import NotFound from './NotFound';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Login from './components/login/Login'
+import { UserStorage } from './UserContext'
+import User from './components/Users/User'
+import ProtectedRoute from './Helpers/ProtectedRoute'
+import NotFound from './NotFound'
+import Photo from './components/Photos/Photo'
+import UserProfile from './components/Users/UserProfile'
 function App() {
   return (
     <div className="App">
@@ -20,6 +22,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/login/*" element={<Login />} />
               <ProtectedRoute path="conta/*" element={<User />} />
+              <Route path="photo/:id" element={<Photo />} />
 
               <Route path="*" element={<NotFound />} />
             </Routes>
@@ -28,7 +31,7 @@ function App() {
         </UserStorage>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
