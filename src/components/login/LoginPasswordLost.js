4637608f06b752d4559c15958becf90ecd2790fc
Bruno Igor <brigor7@gmail.com>
2021-03-10 21:23:55 -0300

    Esqueci a senha: Criado form para envio de login

--- src/components/login/LoginPasswordLost.js
+++ src/components/login/LoginPasswordLost.js
@@ -1,33 +1,59 @@
 import React, { useState } from 'react'
+import useForm from '../../hooks/useForm'
+import useFetch from '../../hooks/useFetch'
+import Input from '../forms/Input'
+import Button from '../forms/Button'
+import Error from '../../Helpers/Error'
 import Head from '../../Helpers/Head'
+import { PASSWORD_LOST } from '../../api'
 
 const LoginPasswordLost = () => {
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
+  const login = useForm()
+  const { data, loading, error, request } = useFetch()
+  console.log('login', login.value)
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
+
+    if (login.validate()) {
+      const { url, options } = await PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace('perdeu', 'resetar'),
+      })
+      const { json } = await request(url, options)
+      console.log('json', json)
+    }
   }
 
   return (
     <section>
       <Head title="Perdeu a senha?" />
-      <h1>Login Perdeu</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          valuer={username}
-          onChange={({ target }) => setUsername(target.value)}
-          placeholder="Username"
-        />
-        <input
-          type="passoword"
-          valuer={email}
-          onChange={({ target }) => setEmail(target.value)}
-          placeholder="Senha"
-        />
-        <button>Entrar</button>
-      </form>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? (
+        <p
+          style={{
+            backgroundColor: '#4c1',
+            color: 'white',
+            textAlign: 'center',
+            fontSize: '1.375rem',
+            padding: '1rem 0',
+            marginTop: '4rem',
+          }}
+        >
+          Email enviado com sucesso!
+        </p>
+      ) : (
+        <form onSubmit={handleSubmit} style={{ marginTop: '4rem' }}>
+          <Input name="login" label="Email ou Usuário" type="text" {...login} />
+
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar Email</Button>
+          )}
+        </form>
+      )}
+      <Error error={error} />
     </section>
   )
 }
